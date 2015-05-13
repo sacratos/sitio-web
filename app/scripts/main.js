0710dc47ae4847b6854b2f1101fa9a102264fb25
sacratos <pablusko_7@hotmail.com>
2015-05-13 19:54:49 +0200

    Prueba formulario

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,30 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+$('#enviar').click(function() {
+    $("#formulario").validate({
+
+        onkeyup: false,
+        onfocusout: false,
+        onclick: false,
+        rules: {
+            nombre: {
+                required: true,
+                lettersonly: true
+            },
+            apellidos: {
+                required: true
+            },
+            telefono: {
+                required: true,
+                digits: true
+            },
+            email1: {
+                required: true,
+                email: true
+            },
+            email2: {
+                required: true,
+                email: true,
+                equalTo: true
+            },
+        }
+    });
+});
