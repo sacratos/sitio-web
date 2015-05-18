9edf296dc282545b6968f272fc11f9767bac8490
sacratos <pablusko_7@hotmail.com>
2015-05-18 19:04:16 +0200

    Prueba con katas.html imagenes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,3 +28,8 @@ $('#enviar').click(function() {
         }
     });
 });
+$("#pop").on("click", function() {
+   $('#imagepreview').attr('src', $('#imageresource').attr('src'));
+   $('#imagemodal').modal('show');
+   
+});
