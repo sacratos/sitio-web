2dde87f1ec3a6bc8dd7b08af50f22f169d43ce98
sacratos <pablusko_7@hotmail.com>
2015-05-25 18:29:22 +0200

    Cambiadas algunas formas de imagenes, problemas con iframe en los videos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,4 @@
+
 $('#formulario').validate({
     onkeyup: false,
     onfocusout: false,
@@ -23,29 +24,30 @@ $('#formulario').validate({
         email2: {
             required: true,
             email: true,
-            equalTo: email,
+            equalTo: 'email',
         },
     },
     messages: {
         nombre: {
-            required: "Escribe tu nombre",
-            lettersonly: "Por favor, sólo caracteres",
+            required: 'Escribe tu nombre',
+            lettersonly: 'Por favor, sólo caracteres',
         },
         apellidos: {
-            required: "Escribe tus apellidos",
+            required: 'Escribe tus apellidos',
         },
         telefono: {
-            minlength: $.validator.format("Por favor, no escribas menos de {0} caracteres."),
-            maxlength: $.validator.format("Por favor, no escribas mas de {0} caracteres."),
-            digits: "Por favor, escribe sólo dígitos.",
+            minlength: $.validator.format('Por favor, no escribas menos de {0} caracteres.'),
+            maxlength: $.validator.format('Por favor, no escribas mas de {0} caracteres.'),
+            digits: 'Por favor, escribe sólo dígitos.',
         },
         email: {
-            required: "Escribe tu email",
-            email: "Por favor, escribe una dirección de correo válida.",
+            required: 'Escribe tu email',
+            email: 'Por favor, escribe una dirección de correo válida.',
         },
         email2: {
-            required: "Escribe tu email",
-            equalTo: "Los emails no coinciden.",
+            required: 'Escribe tu email',
+            email: 'Por favor, escribe una dirección de correo válida.',
+            equalTo: 'Los emails no coinciden.',
         },
 
     }
@@ -53,3 +55,6 @@ $('#formulario').validate({
 $('label').css('font-family','Shojumaru')
     .css('font-size','20px')
     .css('color','#003333');
+
+var player =  iframe.getElementsByTagName('iframe');
+player.mute();
